75c759f37b671bd62af0350eac8630d15b763b02
workanator <workanator@gmail.com>
2017-07-30 23:51:15 +0500

    Add Words example

---
+++ Makefile
@@ -0,0 +1,7 @@
+all:
+	@echo "Please use on of the targets from the list below."
+	@echo "Examples:"
+	@echo "  - ex_words"
+
+ex_words:
+	@go run examples/words/main.go
---
+++ examples/words/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	floc "github.com/workanator/go-floc"
+	"github.com/workanator/go-floc/run"
+)
+
+const exampleText = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+  do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+  minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+  non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+
+var sanitizeWordRe = regexp.MustCompile(`\W`)
+
+func main() {
+	type Statistics struct {
+		Words      []string
+		Characters int
+		Occurence  map[string]int
+	}
+
+	// Split to words and sanitize
+	SplitToWords := func(flow floc.Flow, state floc.State, update floc.Update) {
+		statistics := state.Data().(*Statistics)
+
+		statistics.Words = strings.Split(exampleText, " ")
+		for i, word := range statistics.Words {
+			statistics.Words[i] = sanitizeWordRe.ReplaceAllString(word, "")
+		}
+	}
+
+	// Count and sum the number of characters in the each word
+	CountCharacters := func(flow floc.Flow, state floc.State, update floc.Update) {
+		statistics := state.Data().(*Statistics)
+
+		for _, word := range statistics.Words {
+			statistics.Characters += len(word)
+		}
+	}
+
+	// Count the number unique words
+	CountUniqueWords := func(flow floc.Flow, state floc.State, update floc.Update) {
+		statistics := state.Data().(*Statistics)
+
+		statistics.Occurence = make(map[string]int)
+		for _, word := range statistics.Words {
+			statistics.Occurence[word] = statistics.Occurence[word] + 1
+		}
+	}
+
+	// Print result
+	PrintResult := func(flow floc.Flow, state floc.State, update floc.Update) {
+		statistics := state.Data().(*Statistics)
+
+		fmt.Printf("Words Total               : %d\n", len(statistics.Words))
+		fmt.Printf("Unique Words              : %d\n", len(statistics.Occurence))
+		fmt.Printf("Non-Whitespace Characters : %d\n", statistics.Characters)
+	}
+
+	// Design the job and run it
+	job := run.Sequence(
+		SplitToWords,
+		run.Parallel(
+			CountCharacters,
+			CountUniqueWords,
+		),
+		PrintResult,
+	)
+
+	floc.Run(
+		floc.NewFlow(),
+		floc.NewState(new(Statistics)),
+		nil,
+		job,
+	)
+}
